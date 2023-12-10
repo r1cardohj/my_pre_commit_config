297c1b72ae3c893800d7f3d1291500ede8ebd8db
r1cardohj <2413302357@qq.com>
2023-12-10 19:15:40 +0800

    xxxxx

--- core.py
+++ core.py
@@ -9,5 +9,21 @@ def sub_one(t: int) -> int:
     return t - 1
 
 
-def hell(hello, jsafsa, fasijfoisa, fjasiojf):
+def hell(
+    hello,
+    jsafsa,
+    fasijfoisa,
+    fjasiojf,
+    saifjosaijfsa,
+    jfiasojfsaoifjoisa,
+    jsioafjoiasjfioasjfo,
+    jsaiofjsaofijasiofj,
+    sajfiosajfoiasjfoisaj,
+    sajfiosajfoisajfio,
+    safjioasjfiosjo,
+):
+    pass
+
+
+def hekljas():
     pass
